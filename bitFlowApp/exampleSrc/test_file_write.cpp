163ef579c64589d7ce3dca50e75acf1713673c01
Mark Rivers <rivers@cars.uchicago.edu>
2023-10-18 09:08:17 -0500

    Add more timing granularity; make numFrames an optional argument

--- bitFlowApp/exampleSrc/test_file_write.cpp
+++ bitFlowApp/exampleSrc/test_file_write.cpp
@@ -17,13 +17,18 @@ char buff[XSIZE*YSIZE];
 
 int main(int argc, char *argv[]) {
 
-  epicsTime startTime, endTime;
-  startTime = epicsTime::getCurrent();
+  epicsTime t1, t2, t3, t4;
   int status;
   unsigned long bytesWritten;
+  int numFrames = NUM_FRAMES;
   char *buff = new char[XSIZE*YSIZE];
+  
+  if (argc >= 2) {
+    numFrames = atoi(argv[1]);
+  }
 
-  printf("Creating file\n");
+  printf("Creating file with %d frames\n", numFrames);
+  t1 = epicsTime::getCurrent();
   #ifdef _WIN32
     HANDLE fileHandle;
     fileHandle = CreateFileA(FILE_NAME, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0);
@@ -31,7 +36,9 @@ int main(int argc, char *argv[]) {
     int fileHandle = creat(FILE_NAME, 0777);
   #endif  
   
-  for (int i=0; i<NUM_FRAMES; i++) {
+  printf("Writing data\n");
+  t2 = epicsTime::getCurrent();
+  for (int i=0; i<numFrames; i++) {
     #ifdef _WIN32
       //printf("writing frame %d\n", i);
       status = WriteFile(fileHandle, buff, XSIZE*YSIZE, &bytesWritten, 0);
@@ -39,6 +46,7 @@ int main(int argc, char *argv[]) {
       bytesWritten = write(fileHandle, buff, XSIZE*YSIZE);
     #endif
   }
+  t3 = epicsTime::getCurrent();
     
   printf("Closing file\n");
   #ifdef _WIN32
@@ -46,8 +54,11 @@ int main(int argc, char *argv[]) {
   #else 
     status = close(fileHandle);
   #endif
-  endTime = epicsTime::getCurrent();
-  double elapsedTime = endTime - startTime;
-  printf("Elapsed time: %f, I/O rate = %f MB/s\n", elapsedTime,  double(XSIZE)*YSIZE*NUM_FRAMES/elapsedTime/1024./1024.);
- 
+  t4 = epicsTime::getCurrent();
+  printf("Time to open file:  %f\n", t2-t1);
+  printf("Time to write data: %f\n", t3-t2);
+  printf("Time to close file: %f\n", t4-t3);
+  printf("Total time:         %f\n", t4-t1);
+  printf("I/O rate (MB/s):    %f\n", double(XSIZE)*YSIZE*numFrames/(t3-t2)/1024./1024.);
+
 }
